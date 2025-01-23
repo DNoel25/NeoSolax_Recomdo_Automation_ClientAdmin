484fb5c2bdc449e6352b12882a54bcb752a240e2
Noel Durairaj <d.noel251299@gmail.com>
2025-01-23 09:54:29 +0530

    Tested REC-1744 Written the test cases as in the plan

--- Tests/Test_Search_Term_Pages/test_search_term_pages.py
+++ Tests/Test_Search_Term_Pages/test_search_term_pages.py
@@ -29,86 +29,86 @@ class TestSearchTerms(BaseTest):
             SEOpages_page.is_search_term_pages_heading_there()
             assert SEOpages_page, "Heading is not displayed." # Use the result for the assertion
 
-        # def test_grid_displayed(self):
-        #     SEOpages_page = SearchTermPages(self.driver)
-        #     SEOpages_page.is_grid_displayed()
-        #     # assert SEOpages_page.is_grid_displayed(), "Grid is not displayed on Search Terms page."
-        #
-        # def test_show_entries_options(self):
-        #     print(" ")
-        #     print("Testing Show Entries...")
-        #     SEOpages_page = SearchTermPages(self.driver)
-        #     for value in [10, 25, 50, 100]:
-        #         assert SEOpages_page.set_show_entries(value), f"Failed to set and verify show entries to {value}."
-        #
-        # def test_search_functionality(self):
-        #     SEOpages_page = SearchTermPages(self.driver)
-        #     SEOpages_page.search_in_grid("washing machines")
-        #     # Add assertions for verifying search results in the grid
-        #
-        # def test_pagination(self):
-        #     print(" ")
-        #     print("--------")
-        #     print("Test pagination functionality on the Search Terms Pages Grid..")
-        #
-        #     SEOpages_page = SearchTermPages(self.driver)
-        #     no_of_rows = SEOpages_page.no_of_rows()
-        #     if no_of_rows:
-        #         # Wait for pagination to load
-        #         SEOpages_page.wait_for_pagination()
-        #         time.sleep(2)
-        #
-        #         # Verify initial state: on the first page
-        #         if SEOpages_page.is_previous_disabled() and SEOpages_page.get_active_page() == 1:
-        #             print("Initial state: Previous button is disabled and active page is 1")
-        #         else:
-        #             print(f"Initial state: Previous button status: {SEOpages_page.is_previous_disabled()}, "
-        #                 f"Active page: {SEOpages_page.get_active_page()}.")
-        #             assert False, "Initial state validation failed."
-        #
-        #         # Navigate to the next page and verify
-        #         SEOpages_page.click_next()
-        #         if SEOpages_page.get_active_page() == 2:
-        #             print("After clicking Next: Active page is 2.")
-        #         else:
-        #             print(f"After clicking Next: Active page is {SEOpages_page.get_active_page()}.")
-        #             assert False, "Next button navigation validation failed."
-        #
-        #         # Navigate to a specific page (e.g., page 5) and verify
-        #         target_page = 5
-        #         SEOpages_page.click_page_number(target_page)
-        #         if SEOpages_page.get_active_page() == target_page:
-        #             print(f"After navigating to page {target_page}: Active page is {target_page}.")
-        #         else:
-        #             print(f"After navigating to page {target_page}: Active page is {SEOpages_page.get_active_page()}.")
-        #             assert False, f"Navigation to page {target_page} validation failed."
-        #
-        #         # Navigate back to the previous page and verify
-        #         SEOpages_page.click_previous()
-        #         if SEOpages_page.get_active_page() == target_page - 1:
-        #             print(f"After clicking Previous: Active page is {target_page - 1}.")
-        #         else:
-        #             print(f"After clicking Previous: Active page is {SEOpages_page.get_active_page()}.")
-        #             assert False, f"Previous button navigation validation failed."
-        #
-        #         # Navigate to the last page and verify
-        #         last_page = SEOpages_page.get_last_page_number()
-        #         SEOpages_page.click_page_number(last_page)
-        #         if SEOpages_page.get_active_page() == last_page and SEOpages_page.is_next_disabled():
-        #             print(f"After navigating to the last page {last_page}: Active page is {last_page}, and Next button is disabled.")
-        #         else:
-        #             print(f"After navigating to the last page {last_page}: Active page is {SEOpages_page.get_active_page()}, "
-        #                 f"Next button status: {SEOpages_page.is_next_disabled()}.")
-        #             assert False, f"Last page navigation validation failed."
-        #
-        #         # Navigate back to the first page and verify
-        #         SEOpages_page.click_page_number(1)
-        #         if SEOpages_page.get_active_page() == 1 and SEOpages_page.is_previous_disabled():
-        #             print("After navigating back to the first page: Active page is 1, and Previous button is disabled.")
-        #         else:
-        #             print(f"After navigating back to the first page: Active page is {SEOpages_page.get_active_page()}, "
-        #                 f"Previous button status: {SEOpages_page.is_previous_disabled()}.")
-        #             assert False, "First page navigation validation failed."
+        def test_grid_displayed(self):
+            SEOpages_page = SearchTermPages(self.driver)
+            SEOpages_page.is_grid_displayed()
+            # assert SEOpages_page.is_grid_displayed(), "Grid is not displayed on Search Terms page."
+
+        def test_show_entries_options(self):
+            print(" ")
+            print("Testing Show Entries...")
+            SEOpages_page = SearchTermPages(self.driver)
+            for value in [10, 25, 50, 100]:
+                assert SEOpages_page.set_show_entries(value), f"Failed to set and verify show entries to {value}."
+
+        def test_search_functionality(self):
+            SEOpages_page = SearchTermPages(self.driver)
+            SEOpages_page.search_in_grid("washing machines")
+            # Add assertions for verifying search results in the grid
+
+        def test_pagination(self):
+            print(" ")
+            print("--------")
+            print("Test pagination functionality on the Search Terms Pages Grid..")
+
+            SEOpages_page = SearchTermPages(self.driver)
+            no_of_rows = SEOpages_page.no_of_rows()
+            if no_of_rows:
+                # Wait for pagination to load
+                SEOpages_page.wait_for_pagination()
+                time.sleep(2)
+
+                # Verify initial state: on the first page
+                if SEOpages_page.is_previous_disabled() and SEOpages_page.get_active_page() == 1:
+                    print("Initial state: Previous button is disabled and active page is 1")
+                else:
+                    print(f"Initial state: Previous button status: {SEOpages_page.is_previous_disabled()}, "
+                        f"Active page: {SEOpages_page.get_active_page()}.")
+                    assert False, "Initial state validation failed."
+
+                # Navigate to the next page and verify
+                SEOpages_page.click_next()
+                if SEOpages_page.get_active_page() == 2:
+                    print("After clicking Next: Active page is 2.")
+                else:
+                    print(f"After clicking Next: Active page is {SEOpages_page.get_active_page()}.")
+                    assert False, "Next button navigation validation failed."
+
+                # Navigate to a specific page (e.g., page 5) and verify
+                target_page = 5
+                SEOpages_page.click_page_number(target_page)
+                if SEOpages_page.get_active_page() == target_page:
+                    print(f"After navigating to page {target_page}: Active page is {target_page}.")
+                else:
+                    print(f"After navigating to page {target_page}: Active page is {SEOpages_page.get_active_page()}.")
+                    assert False, f"Navigation to page {target_page} validation failed."
+
+                # Navigate back to the previous page and verify
+                SEOpages_page.click_previous()
+                if SEOpages_page.get_active_page() == target_page - 1:
+                    print(f"After clicking Previous: Active page is {target_page - 1}.")
+                else:
+                    print(f"After clicking Previous: Active page is {SEOpages_page.get_active_page()}.")
+                    assert False, f"Previous button navigation validation failed."
+
+                # Navigate to the last page and verify
+                last_page = SEOpages_page.get_last_page_number()
+                SEOpages_page.click_page_number(last_page)
+                if SEOpages_page.get_active_page() == last_page and SEOpages_page.is_next_disabled():
+                    print(f"After navigating to the last page {last_page}: Active page is {last_page}, and Next button is disabled.")
+                else:
+                    print(f"After navigating to the last page {last_page}: Active page is {SEOpages_page.get_active_page()}, "
+                        f"Next button status: {SEOpages_page.is_next_disabled()}.")
+                    assert False, f"Last page navigation validation failed."
+
+                # Navigate back to the first page and verify
+                SEOpages_page.click_page_number(1)
+                if SEOpages_page.get_active_page() == 1 and SEOpages_page.is_previous_disabled():
+                    print("After navigating back to the first page: Active page is 1, and Previous button is disabled.")
+                else:
+                    print(f"After navigating back to the first page: Active page is {SEOpages_page.get_active_page()}, "
+                        f"Previous button status: {SEOpages_page.is_previous_disabled()}.")
+                    assert False, "First page navigation validation failed."
 
         # def test_filters(self):
         #     print(" ")
@@ -158,6 +158,7 @@ class TestSearchTerms(BaseTest):
             # upload_img.delete_image()
             # assert upload_img.is_image_deleted(), "Image was not deleted successfully."
 
+    #######------stopped due to an issue raising to fill in the frame------#######
         # def test_write_content(self):
         #     """
         #             Tests entering a description inside the note-editor note-frame.
@@ -185,7 +186,12 @@ class TestSearchTerms(BaseTest):
             fill_seo.fill_seo_fields()
 
         def test_redirecttoproductgridandsortin_and_save(self):
-            redirect_and_save = SearchTermPages(self.driver)
+            redirect_save = SearchTermPages(self.driver)
+            redirect_save.redirection_save()
+
+        def test_save(self):
+            save = SearchTermPages(self.driver)
+            save.save()
 
 
     except Exception as e:
